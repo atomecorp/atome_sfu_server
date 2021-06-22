4c162d5028a4947ddeab673a3189f0df850a6dbc
Régis Ramillien <r.ramillien@tritonit.tech>
2021-06-22 22:16:07 +0200

    Continued simplification.

--- app/lib/index.jsx
+++ app/lib/index.jsx
@@ -1,39 +1,36 @@
 import domready from 'domready';
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import {
-	createStore as createReduxStore
-} from 'redux';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore as createReduxStore} from 'redux';
 
 import RoomClient from './RoomClient';
 import reducers from './redux/reducers';
-import Peers from './components/Peers';
+import App from './components/App';
 import randomString from "random-string";
 
 let roomClient;
 const store = createReduxStore(
-	reducers
+    reducers
 );
 
-RoomClient.init({ store });
+RoomClient.init({store});
 
-domready(async () =>
-{
-	roomClient = new RoomClient(
-		{
-			roomId : 0,
-			peerId : randomString()
-		});
+domready(async () => {
+    roomClient = new RoomClient(
+        {
+            roomId: 0,
+            peerId: randomString()
+        });
 
-	render(
-		<Provider store={store}>
-			<div data-component='Room'>
-				<Peers />
-			</div>
-		</Provider>,
-		document.getElementById('mediasoup-demo-app-container')
-	);
+    render(
+        <Provider store={store}>
+            <div data-component='Room'>
+                <App/>
+            </div>
+        </Provider>,
+        document.getElementById('mediasoup-demo-app-container')
+    );
 
-	roomClient.join();
+    roomClient.join();
 });
